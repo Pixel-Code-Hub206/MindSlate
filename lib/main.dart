e9dee4ab769778e6f56203c62a753a21f25ee460
Pixel-Code-Hub206 <mayanklan206@gmail.com>
2025-09-29 01:08:21 +0530

    Changed the folder structure for it to be more unambigous

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:mindslate/app.dart';
+import 'package:mindslate/pages/home_screen.dart';
 
 void main() => runApp(
     DevicePreview(
-      enabled: true,
-       builder: (context) => MindSlate()
+      enabled: kDebugMode,
+       builder: (context) => Home()
     ),
 );
---
+++ lib/pages/home_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:mindslate/utils/theme/app_theme.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      builder: DevicePreview.appBuilder,
+      themeMode: ThemeMode.system,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('MindSlate'),
+        ),
+        body: Center(child: Text("This is the First MindSlate. Get Ready....")),
+      ),
+    );;
+  }
+}
