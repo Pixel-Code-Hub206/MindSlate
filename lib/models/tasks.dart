c8124ae3c274d7a8cfcabeab6bd70fe64378c451
Pixel-Code-Hub206 <mayanklan206@gmail.com>
2025-10-21 16:29:35 +0530

    Finalized the Icon Launcher/App logo. Changed the medium priority logo.

--- lib/models/tasks.dart
+++ lib/models/tasks.dart
@@ -6,7 +6,7 @@ part 'tasks.g.dart';
 enum Priority{
   urgent(color: Color(0XFFFF3232), pTitle: 'Urgent'),  //Priority defined in the form of a Priority instance
   high(color: Color(0xFFFF961E), pTitle: 'High'),
-  medium(color: Color(0xFFF4FC17), pTitle: 'Medium'),
+  medium(color: Color(0xFFF1D201), pTitle: 'Medium'),
   low(color: Color(0xFF3BFF48), pTitle: 'Low');
 
   final Color color;
--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -32,7 +32,7 @@ class _HomeState extends State<Home> {
     tasksBox.add(
       Tasks(
         isTitle: 'Hello there!',
-        isBody: 'Welcome to Mindslate!',
+        isBody: 'Welcome to MindSlate!',
         isDone: false,
         priority: 'medium',
       ),
